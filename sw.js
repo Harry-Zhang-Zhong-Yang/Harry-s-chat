ab906702f6badb5df2f1faf6498c4bfb970309f8
Harry-Zhang-Zhong-Yang <ha20100120@163.com>
2026-08-20 14:44:43 +0800

    Add message event listener to skip waiting

--- sw.js
+++ sw.js
@@ -4,6 +4,10 @@ self.addEventListener('install', (event) => {
     event.waitUntil(self.skipWaiting());
 });
 
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim());
 });
